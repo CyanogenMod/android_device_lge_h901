ffabe20f3ef4ead45332a731c8decd0bec300e51
Jake Whatley <jacob.whatley91@gmail.com>
2016-02-13 23:21:14 -0500

    h901: merge with v10-common
    h901 (T-Mobile US) is the only unlockable variant,
    there is absolutely no need for a common device repo
    
    Change-Id: I4b185ac6c0cce01e8d197900d792165f5727851d
    
    h901: update partition sizes
    
    - add cache and persist
    
    h901: remove liblights and LED overlays
    
    v10 does not have an LED
    
    update for mm
    
    Change-Id: I7c1643b538036bd6e483191b0530665e4e5eb552
    
    Revert "g4: gps: update hal from caf"
    
    This reverts commit 6364b25.
    
    h901: fix leftovers from g4-common
    
    Change-Id: I4b185ac6c0cce01e8d197900d792165f5727851d

--- cm.mk
+++ cm.mk
@@ -1,11 +1,10 @@
-$(call inherit-product, device/lge/h901/full_h901.mk)
+$(call inherit-product, device/lge/h901/aosp_h901.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 
 PRODUCT_NAME := cm_h901
 
@@ -14,3 +13,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="pplus_tmo_us" \
     BUILD_FINGERPRINT="lge/pplus_tmo_us/pplus:5.1.1/LMY47V/152801355bef0:user/release-keys" \
     PRIVATE_BUILD_DESC="pplus_tmo_us-user 5.1.1 LMY47V 152801355bef0 release-keys"
+
